0ba4e1b4d91014ebc82f6fba50d58dedc6e72211
Andrew Mundy <andrewmundy@gmail.com>
2024-11-11 15:34:04 -0800

    feat: add emoji

--- app/components/EntryList.tsx
+++ app/components/EntryList.tsx
@@ -11,74 +11,76 @@ interface EntryListProps {
 export default function EntryList({ entries }: EntryListProps) {
   return (
     <div className="space-y-5">
-      {entries.map(
-        (
-          {
-            title,
-            article,
-            image,
-            body,
-            price,
-            publishedAt,
-            priceCalculation,
-            active = true,
-            shouldCalculatePrice = true,
-          },
-          i
-        ) => (
-          <div key={i} className="rounded-md border-gray-200 border">
-            <div className="flex flex-row">
-              {image && (
-                <div
-                  className="relative rounded-lt-md rounded-tr-md"
-                  style={{ height: "auto", minWidth: 200 }}
-                >
-                  <Image
-                    fill={true}
-                    style={{
-                      borderRadius: "0.375rem 0 0 0.375rem",
-                    }}
-                    src={image}
-                    alt={title ?? ""}
-                  />
-                </div>
-              )}
-
-              <div className="p-4">
-                {publishedAt && (
-                  <p className="text-sm text-gray-500 dark:text-gray-400">
-                    {new Date(publishedAt).toLocaleDateString()}
-                  </p>
-                )}
-                <div className="flex-col ">
-                  <p className="text-2xl font-bold text-red-600 dark:text-red-400">
-                    {shouldCalculatePrice
-                      ? `-${formatCurrency(price ?? 0)}`
-                      : "Incalculable"}
-                  </p>
-                  <div>
-                    {priceCalculation && (
-                      <i className="text-sm text-gray-400 dark:text-gray-300">
-                        {priceCalculation}
-                      </i>
-                    )}
+      {entries
+        .filter((entry) => entry.active)
+        .map(
+          (
+            {
+              title,
+              article,
+              image,
+              body,
+              price,
+              publishedAt,
+              priceCalculation,
+              active = true,
+              shouldCalculatePrice = true,
+            },
+            i
+          ) => (
+            <div key={i} className="rounded-md border-gray-200 border">
+              <div className="flex flex-row">
+                {image && (
+                  <div
+                    className="relative rounded-lt-md rounded-tr-md"
+                    style={{ height: "auto", minWidth: 200 }}
+                  >
+                    <Image
+                      fill={true}
+                      style={{
+                        borderRadius: "0.375rem 0 0 0.375rem",
+                      }}
+                      src={image}
+                      alt={title ?? ""}
+                    />
                   </div>
-                  <div className="mt-4">
-                    <a
-                      href={article}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-                    >
-                      {title}
-                    </a>
+                )}
+
+                <div className="p-4">
+                  {publishedAt && (
+                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                      {new Date(publishedAt).toLocaleDateString()}
+                    </p>
+                  )}
+                  <div className="flex-col ">
+                    <p className="text-2xl font-bold text-red-600 dark:text-red-400">
+                      {shouldCalculatePrice
+                        ? `-${formatCurrency(price ?? 0)}`
+                        : "Incalculable"}
+                    </p>
+                    <div>
+                      {priceCalculation && (
+                        <i className="text-sm text-gray-400 dark:text-gray-300">
+                          {priceCalculation}
+                        </i>
+                      )}
+                    </div>
+                    <div className="mt-4">
+                      <a
+                        href={article}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
+                      >
+                        {title}
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        )
-      )}
+          )
+        )}
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🥚</text></svg>"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
